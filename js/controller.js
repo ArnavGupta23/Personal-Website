d8de5cc2f4c7e16f108064875cd96b94eb4724a4
ArnavGupta23 <93295202+ArnavGupta23@users.noreply.github.com>
2023-06-24 18:16:11 -0400

    favcon update

--- js/controller.js
+++ js/controller.js
@@ -3,3 +3,17 @@ function scrollToSection(sectionId) {
     var section = document.getElementById(sectionId);
     section.scrollIntoView({ behavior: 'smooth' });
 }
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    document.getElementById(tabname).classList.add("active-tab");
+    event.currentTarget.classList.add("active-link");
+}
